dcc248f6e08c7ecfdbfa79cdea03a6a153749698
ZuLu29 <markneilconde850@gmail.com>
2024-12-02 18:43:20 +0800

    adding tryal html fprm

--- index.php
+++ index.php
@@ -1,1 +1,35 @@
-<?php echo "helooo"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MATH QUIZ</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="lvl1">Level 1</label>
+        <input type="radio" id="lvl1" name="lvl"><br>
+        <label for="lvl2">Level 2</label>
+        <input type="radio" id="lvl2" name="lvl2"><br>
+        <label for="customlvl">Custom Level</label>
+        <input type="radio" id="customlvl" name="dustomlvl"><br>
+        <input type="number" id="number" name="number">
+        <input type="number" id="number" name="number">
+    </form>
+    <form action=""method="post">
+    <label for="lvl1">Addition</label>
+        <input type="radio" id="lvl1" name="lvl"><br>
+        <label for="lvl2">subtraction</label>
+        <input type="radio" id="lvl2" name="lvl2"><br>
+        <label for="customlvl">multiplication</label>
+        <input type="radio" id="customlvl" name="dustomlvl"><br>
+    </form>
+    <form action="" method="post">
+        <label for="noi">Number of items</label>
+        <input type="number" name="noi"><br>
+        <label for="max">Max Difference of choices from the correct answer</label>
+        <input type="number" name="max">
+    </form>
+    <a href="questions.php">start the quiz</a>
+</body>
+</html>
