f62ea86d81fa68b0a565f0777e405363bee5d503
ZuLu29 <markneilconde850@gmail.com>
2024-12-03 21:03:11 +0800

    adding html that display the questions

--- quiz.php
+++ quiz.php
@@ -15,3 +15,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>math quiz</title>
+</head>
+<body>
+    
+</body>
+</html>
