a9a61915dbc796c32ca733f47c982af665f03093
ZuLu29 <markneilconde850@gmail.com>
2024-12-03 20:46:09 +0800

    adding form for user input

--- index.php
+++ index.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SETTINGS</title>
+    <title>MATH QUIZ SETTINGS</title>
 </head>
 <body>
-    
+    <h2>Math Quiz Settings</h2>
+    <form action="" method="POST">
+        
+    </form>
 </body>
 </html>
