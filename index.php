c18640c142cab1b56cb3e9f275b06320538055eb
ZuLu29 <markneilconde850@gmail.com>
2024-12-03 20:52:03 +0800

    adding operators

--- index.php
+++ index.php
@@ -8,14 +8,21 @@
 <body>
     <h2>Math Quiz Settings</h2>
     <form action="" method="POST">
-    <label for="level">Select Level:</label>
-        <select name="level" id="level" required>
-            <option value="1">Level 1 (1-10)</option>
-            <option value="2">Level 2 (11-100)</option>
+        <label for="level">Select Level:</label>
+            <select name="level" id="level" required>
+                <option value="1">Level 1 (1-10)</option>
+                <option value="2">Level 2 (11-100)</option>
+            </select>
+            <br><br>
+
+        <label for="operator">Choose Operator:</label>
+        <select name="operator" id="operator" required>
+            <option value="add">Addition (+)</option>
+            <option value="subtract">Subtraction (-)</option>
+            <option value="multiply">Multiplication (×)</option>
         </select>
         <br><br>
 
-        
 
     </form>
 </body>
